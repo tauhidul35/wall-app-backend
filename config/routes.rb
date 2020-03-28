91445319690d878357e7a64a33df274693d23381
Tauhidul Islam <tauhidul@misfit.tech>
2020-03-28 14:53:16 +0600

    Remove devise default routes and add authentication api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post 'authenticate', to: 'authentication#authenticate'
       resources :posts
     end
   end
