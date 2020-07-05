6d186157be52640a59b206becfe2ecd443054fb7
Tauhidul Islam <tauhidul@misfit.tech>
2020-07-06 05:04:13 +0600

    Send welcome email on user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :posts
 
   devise :database_authenticatable, :registerable, :recoverable, :validatable
+
+  after_create :send_admin_mail
+
+  private
+
+  def send_admin_mail
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,20 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = {from: 'no-reply@wall-app.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'tauhidul.test',
+      password:             'Tauhidul@404',
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
